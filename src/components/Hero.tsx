9ae49649c2cad3b16e355f0c17218445b0c3f038
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2025-05-16 18:54:16 +0300

    Refactored Hero component for improved text gradient colors and layout adjustments

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,31 +14,30 @@ export default function Hero() {
         <Image
           src={heroBackgroundImage}
           alt="Hero Background"
-          layout="fill"
+          fill
           objectFit="cover"
           priority
         />
       </div>
 
-      <div className="w-full md:w-1/2 z-20 flex flex-col justify-center min-h-[40vh] p-4 pt-20 md:p-8 md:pl-40 text-center md:text-left">
-        <p className="text-xl md:text-2xl font-bold mb-4 md:mb-8 bg-gradient-to-r from-gray-700 via-gray-400 to-gray-200 md:bg-none text-transparent md:text-black bg-clip-text md:bg-clip-border">
+      <div className="w-full md:w-1/2 z-20 flex flex-col justify-center min-h-[40vh] p-4 pt-20 md:p-40 text-center md:text-left">
+        <p className="text-xl md:text-2xl font-bold mb-4 md:mb-8 bg-gradient-to-r from-gray-800 via-gray-300 to-gray-200 md:bg-none text-transparent md:text-gray-800 bg-clip-text md:bg-clip-border">
           Hi, I am
         </p>
-        <h1 className="text-3xl md:text-4xl font-bold mb-2 bg-gradient-to-r from-gray-700 via-gray-400 to-gray-200 md:bg-none text-transparent md:text-black bg-clip-text md:bg-clip-border">
+        <h1 className="text-3xl md:text-4xl font-bold mb-2 bg-gradient-to-r from-gray-800 via-gray-400 to-gray-200 md:bg-none text-transparent md:text-gray-800 bg-clip-text md:bg-clip-border">
           Mohamad Sharafeddine
         </h1>
-        <p className="text-base md:text-lg font-bold bg-gradient-to-r from-gray-700 via-gray-400 to-gray-200 md:bg-none text-transparent md:text-gray-500 bg-clip-text md:bg-clip-border">
+        <p className="text-base md:text-lg font-bold bg-gradient-to-r from-gray-800 via-gray-400 to-gray-200 md:bg-none text-transparent md:text-gray-500 bg-clip-text md:bg-clip-border">
           Full-Stack Web & Mobile Developer
         </p>
       </div>
 
-      <div className="relative z-20 min-w-[400px] min-h-[400px] max-w-[800px] max-h-[800px] md:min-w-[600px] md:min-h-[600px] md:max-w-[800px] md:max-h-[800px] mx-auto md:mr-20 self-end">
+      <div className="relative z-20 min-w-[400px] min-h-[533px] max-w-[600px] max-h-[800px] md:min-w-[450px] md:min-h-[600px] md:max-w-[600px] md:max-h-[800px] mx-auto self-end">
         <Image
           src={personalImage}
           alt="Personal Image"
           fill
           quality={100}
-          sizes="(max-width: 768px) 800px, 800px"
           className="object-contain mix-blend-multiply"
           priority
         />
